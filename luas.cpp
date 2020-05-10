b1c4222f085e5214218e84fb27390ea2cea0f182
Dimzkaz <65090600+Dimzkaz@users.noreply.github.com>
2020-05-10 15:15:18 +0700

    Add files via upload

---
+++ luas.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h> // Preprocessor Directive
+int main(void) //Function utama
+{ //Blok pembuka
+    int d = 100;
+	int e = 50;
+	int f = 40;
+	int luas = d*e*f;
+	printf("luas dari balok dengan panjang = %d , lebar = %d , tinggi = %d adalah %d", d, e, f, luas);  //Pernyataan (Statement)
+    return 0; //Pernyataan
+} //Blok Penutup
